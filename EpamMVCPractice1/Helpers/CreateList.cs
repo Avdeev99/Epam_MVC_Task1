d63cb14780755249fe35fe096db8561c492681d6
Avdeev99 <50865612+Avdeev99@users.noreply.github.com>
2019-05-21 19:50:28 +0300

    initial

---
+++ EpamMVCPractice1/Helpers/CreateList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EpamMVCPractice1.Helpers
+{
+    public static class CreateList
+    {
+
+        public static MvcHtmlString CreateL(this HtmlHelper html, IEnumerable<string> items)
+        {
+            TagBuilder ul = new TagBuilder("ul");
+            foreach (string item in items)
+            {
+                TagBuilder li = new TagBuilder("li");
+                li.SetInnerText(item);
+                ul.InnerHtml += li.ToString();
+            }
+            return new MvcHtmlString(ul.ToString());
+        }
+    }
+}
---
+++ EpamMVCPractice1/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EpamMVCPractice1.Models
+{
+    public class Comment
+    {
+        public int commentId { get; set; }
+        public string authorName { get; set; }
+        public DateTime Date { get; set; }
+        public string commentContent { get; set; }
+    }
+}
